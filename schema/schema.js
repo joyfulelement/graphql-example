b054b71296cd908f465ca5719f2b7dab2165b0b3
Kevin C Chen (RD-CA) <kevin_c_chen@trendmicro.com>
2017-07-10 17:56:39 -0400

    Associate Company type back to many User type

--- schema/schema.js
+++ schema/schema.js
@@ -8,21 +8,29 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({ //JS closure ( to resolve circular reference - UserType) - defined first but does not get executed until after the entire file gets executed
         id: {type: GraphQLString},
         name: {type: GraphQLString},
-        description: {type: GraphQLString}
-    }
+        description: {type: GraphQLString},
+        users: {
+            type: new GraphQLList(UserType), //multiple users associated with the company
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(resp => resp.data)
+            }
+        }
+    })
 })
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: {type: GraphQLString},
         firstName:{type: GraphQLString},
         age:{type: GraphQLInt},
@@ -34,7 +42,7 @@ const UserType = new GraphQLObjectType({
                     .then(resp => resp.data)
             }
         }
-    }
+    })
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -45,9 +53,16 @@ const RootQuery = new GraphQLObjectType({
             args: {id:{type: GraphQLString}},
             resolve(pcarentValue, args) {
                 //Go to any data store and find tha actual data we are looking for
-                // return _.find(users, {id:args.id}) //types handled by GraphQL
                 return axios.get(`http://localhost:3000/users/${args.id}`)
                     .then(resp => resp.data) //to access the data property return from axios library
+            }
+        },
+        company: {
+            type: CompanyType,
+            args:{id:{type:GraphQLString}},
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                .then(resp => resp.data)
             }
         }
     }
